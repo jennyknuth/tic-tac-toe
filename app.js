fb163fa65bfa6b3da92f2782c6e858f1863c91c8
Jenny Knuth <inkboco@gmail.com>
2015-06-18 17:17:20 -0600

    linted a bit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var div = document.getElementsByTagName('div')
 var board = document.getElementById('board')
 
-var win = [[a1,a2,a3], [b1,b2,b3], [c1,c2,c3], [a1,b1,c1], [a2,b2,c2], [a3,b3,c3], [a1,b2,c3], [a3,b2,c1]]
+var win = [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3'], ['c1', 'c2', 'c3'], ['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3'], ['a1', 'b2', 'c3'], ['a3', 'b2', 'c1']]
 
 var o = {}
 var x = {}
@@ -10,21 +10,29 @@ var player = 'O'
 
 console.log(div)
 
+var checkWinner = function (player) {
+  win.forEach(function (element) {
+    for (key in o === element) {
+      console.log(element, 'o wins?')
+
+    }
+  })
+}
+
 var handleEvent = function (event) {
   if (player === 'O' && event.target.innerHTML === '') {
     o[event.target.id] = 'O'
     event.target.innerHTML = 'O'
     console.log(o)
     player = 'X'
-    checkWinner()
-  } else if (player === 'X' && event.target.innerHTML === ''){
+    checkWinner(player)
+  } else if (player === 'X' && event.target.innerHTML === '') {
     x[event.target.id] = 'X'
     event.target.innerHTML = 'X'
     console.log(x)
     player = 'O'
-    checkWinner()
+    checkWinner(player)
   }
-
 }
 
 Array.prototype.forEach.call(div, function (element) {
