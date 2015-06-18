f4297c8bdd17a6d933f9bec45c6abab3899783b4
Jenny Knuth <inkboco@gmail.com>
2015-06-18 16:55:58 -0600

    got X and O going in boxes and objects

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
+
 var div = document.getElementsByTagName('div')
 var board = document.getElementById('board')
 
-var win = {
-}
+var win = [[a1,a2,a3], [b1,b2,b3], [c1,c2,c3], [a1,b1,c1], [a2,b2,c2], [a3,b3,c3], [a1,b2,c3], [a3,b2,c1]]
 
 var o = {}
 var x = {}
+var player = 'O'
 
 console.log(div)
+
 var handleEvent = function (event) {
-  o[event.target.id] = 1
-  event.target.class = "one"
+  if (player === 'O' && event.target.innerHTML === '') {
+    o[event.target.id] = 'O'
+    event.target.innerHTML = 'O'
+    console.log(o)
+    player = 'X'
+    checkWinner()
+  } else if (player === 'X' && event.target.innerHTML === ''){
+    x[event.target.id] = 'X'
+    event.target.innerHTML = 'X'
+    console.log(x)
+    player = 'O'
+    checkWinner()
+  }
+
 }
 
-div.forEach(function (element) {
-  element.addEventListener('click', function(event){
-    console.log("box was clicked")
-    console.log(event.target.id)
-    event.target.call(handleEvent)
+Array.prototype.forEach.call(div, function (element) {
+  element.addEventListener('click', function (event) {
+    console.log(event.target)
+    handleEvent(event)
   })
 })
-
-
-module.exports = {
-
-}
--- spec/spec.js
+++ spec/spec.js
@@ -1,6 +1,7 @@
-var code = require(./../app.js)
+var code = require('./../app.js')
 
 describe('#ticTACtoe', function(){
  it('plays tic tac toe', function() {
    expect(code.play(1)).toEqual(2);
+ });
 });
